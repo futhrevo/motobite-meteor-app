2986f505be945ace72c5fd8b044595c3bd28c280
futhrevo <k.rakeshlal@gmail.com>
2014-11-19 21:28:13 +0530

    Added marker and some helper files

---
+++ client/helpers/gmap_helpers.js
@@ -0,0 +1,65 @@
+//http://www.andrehonsberg.com/article/reactive-google-maps-markers-meteor-js
+gmap = {
+	//map object
+	map: null,
+
+	//google markers objects
+	markers: [],
+
+	//google lat lng objects
+	latLngs: [],
+
+	//formatted marker data objects
+	markerData: [],
+
+	//add a marker from the given marker object
+	addMarker: function(marker) {
+		var myLatlng = new google.maps.LatLng(marker.lat,marker.lng);
+		// To add the marker to the map, use the 'map' property
+		var mymarker = new google.maps.Marker({
+		    position: myLatlng,
+		    map: this.map,
+		    title:marker.title,
+		    animation: google.maps.Animation.DROP,
+		    icon:'/taxi.png'
+		});
+
+		//keep track of markers and geo data
+		this.latLngs.push(myLatlng);
+		this.markers.push(mymarker);
+		this.markerData.push(marker);
+
+		return mymarker;
+	},
+
+	// calculate and move the bound box based on our markers
+	calcBounds: function(){
+		var bounds = new google.maps.LatLngBounds();
+		for (var index in this.latLngs){
+			bounds.extend(this.latLngs[index]);
+		}
+		this.map.fitBounds(bounds);
+	},
+
+	//check if marker already exists
+	markerExists: function(key, val){
+		_.each(this.markers, function(storedMarker){
+			if(storedMarker[key] == val)
+				return true;
+		});
+		return false;
+	},
+
+	//initialize the map
+	initialize: function(){
+		console.log("[+] Initializing Google Maps...");
+		var mapOptions ={
+			center: new google.maps.LatLng(12.9525812,77.7034538),
+			zoom: 13
+		};
+		this.map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+
+		//global flag saying we initialized already
+		Session.set('map', true);
+	}
+}
--- client/templates/disp_map.js
+++ client/templates/disp_map.js
@@ -1,9 +1,18 @@
 Template.dispMap.rendered = function(){
-	// map initialization
-	var mapOptions ={
-		center: {lat:12.9525812, lng:77.7034538},
-		zoom: 13
+	if(! Session.get('map'))
+		gmap.initialize();
+
+	var objMarker = {
+		id: 1,
+		lat: 12.9525812,
+		lng: 77.7034538,
+		title: "Home"
 	};
-	var map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
-	console.log("Map helper Loaded");
+
+	//check if marker already exists
+	if(! gmap.markerExists('id',objMarker.id))
+		gmap.addMarker(objMarker);
+}
+Template.dispMap.destroyed = function(){
+	Session.set('map', false);
 }
