0939fe2c285d8f7445bc19cdf05fe73a2b8ecf86
futhrevo <k.rakeshlal@gmail.com>
2014-11-20 21:35:29 +0530

    read location from db

--- client/templates/disp_map.js
+++ client/templates/disp_map.js
@@ -2,16 +2,14 @@ Template.dispMap.rendered = function(){
 	if(! Session.get('map'))
 		gmap.initialize();
 
-	var objMarker = {
-		id: 1,
-		lat: 12.9525812,
-		lng: 77.7034538,
-		title: "Home"
-	};
-
-	//check if marker already exists
-	if(! gmap.markerExists('id',objMarker.id))
-		gmap.addMarker(objMarker);
+	var locs = Posts.find().fetch();
+	for(var loc in locs){
+		var objMarker = locs[loc];
+		console.log(objMarker);
+		//check if marker already exists
+		if(! gmap.markerExists('id',objMarker.id))
+			gmap.addMarker(objMarker);
+	}	
 }
 Template.dispMap.destroyed = function(){
 	Session.set('map', false);
