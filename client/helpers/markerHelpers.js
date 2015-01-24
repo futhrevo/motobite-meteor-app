42f6b1afa2d1bebbb1908af41781256ec3b13c41
Rakesh Kalyankar <k.rakeshlal@gmail.com>
2015-01-25 00:03:39 +0530

    marker manupulations

--- client/helpers/markerHelpers.js
+++ client/helpers/markerHelpers.js
@@ -14,7 +14,7 @@ MarkerManager.prototype.initialize = function (opt_opts) {
 };
 
 //Prototype to add markers to manager
-MarkerManager.prototype.addMarker = function(loc,options,type){
+MarkerManager.prototype.addMarker = function(loc,options){
   var me = this;
   options = options || {};
   var opts = {};
@@ -22,7 +22,13 @@ MarkerManager.prototype.addMarker = function(loc,options,type){
   opts.position = new google.maps.LatLng(loc.lat,loc.lng);
   opts.title = loc.title || "TODO";
   var mymarker = me.Marker_(opts);
-
+  if(options.hasOwnProperty("type")){
+    if(options["type"] === "driver"){
+      driversMarkers[loc.id] = mymarker;
+    }else{
+      drivesMarkers[loc.id] = mymarker;
+    }
+  }
 
 };
 
@@ -43,15 +49,37 @@ MarkerManager.prototype.Marker_ = function(opts){
 
 //Prototype to remove all markers
 MarkerManager.prototype.clearMarkers = function(){
+  for(var id in drivesMarkers){
+    drivesMarkers[id]["setMap"](null);
+    delete drivesMarkers[id];
+  }
 
+  for(var id in driversMarkers){
+    driversMarkers[id]["setMap"](null);
+    delete drivesMarkers[id];
+  }
 };
 
 //Prototype to remove one marker
-MarkerManager.prototype.delMarker = function(){
-
+MarkerManager.prototype.delMarker = function(id){
+  if (drivesMarkers.hasOwnProperty(id)) {
+    drivesMarkers[id]["setMap"](null);
+    delete drivesMarkers[id];
+  }
+  if (driversMarkers.hasOwnProperty(id)) {
+    driversMarkers[id]["setMap"](null);
+    delete driversMarkers[id];
+  }
 };
 
 //Protype to query a user to get marker
-MarkerManager.prototype.queryUser = function(){
-
+MarkerManager.prototype.queryUser = function(id){
+  var retMarker={};
+  if (drivesMarkers.hasOwnProperty(id)) {
+    retMarker.drives = drivesMarkers[id];
+  }
+  if (driversMarkers.hasOwnProperty(id)) {
+    retMarker.drivers = driversMarkers[id];
+  }
+  return retMarker;
 };
