46034c399f5d7ebc629cc0fab9ce65b73243a0b7
Rakesh Kalyankar <rakeshkalyankar@Rakeshs-Mac-mini.local>
2015-06-13 15:05:18 +0530

    before local cordova plugin

--- server/access-control.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by rakeshkalyankar on 21/05/15.
- */
-// CORS headers...  lets just let them all in, shall we?
-//WebApp.connectHandlers.use('/api/geolocation', function(req, res, next) {
-//    res.setHeader("Access-Control-Allow-Origin", "*");
-//    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//    return next();
-//});
