d7af340f712a347d2575d5c50abfe2447d612ede
Rakesh Kalyankar <rakeshkalyankar@Rakeshs-Mac-mini.local>
2015-08-05 12:14:25 +0530

    first icons for motobite

--- mobile-config.js
+++ mobile-config.js
@@ -2,7 +2,7 @@
 // the entire section is optional.
 App.info({
   id: 'com.futrevo.reku.motobite',
-  name: 'motobite',
+  name: 'Motobite',
   description: 'Future Evolved',
   author: 'Rakesh Kalyankar',
   email: 'admin@motobite.com',
@@ -17,3 +17,13 @@ App.accessRule('https://*.google.com/*');
 
 App.setPreference("StatusBarBackgroundColor", "#00796B");
 
+App.icons({
+    'iphone_2x': 'resources/icons/appicon-60@2x.png', //120x120
+    'iphone_3x': 'resources/icons/appicon-60@3x.png', //180x180
+    'ipad': 'resources/icons/appicon-76.png', //76x76
+    'ipad_2x': 'resources/icons/appicon-76@2x.png', //152x152
+    'android_ldpi': 'resources/icons/drawable-ldpi/appicon.png', //36x36
+    'android_mdpi': 'resources/icons/drawable-mdpi/appicon.png', //48x48
+    'android_hdpi': 'resources/icons/drawable-hdpi/appicon.png', //72x72
+    'android_xhdpi': 'resources/icons/drawable-xhdpi/appicon.png', //96x96
+});
