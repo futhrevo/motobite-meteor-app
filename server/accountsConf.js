6f0a2b38bfec5d441906f1d0a191debf9441b762
Rakesh Kalyankar <k.rakeshlal@gmail.com>
2016-05-09 22:18:08 +0530

    fb login behaviour handler

--- server/accountsConf.js
+++ server/accountsConf.js
@@ -1,4 +1,7 @@
 /* global Accounts */
+import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
+import { HTTP } from 'meteor/http';
 //configure user accounts
 Accounts.config({
     //New users with an email address will receive an address verification email
@@ -67,7 +70,7 @@ Accounts.validateNewUser(function (user) {
 // Validate username, without a specific error message.
 //TODO: Mechanism for users to report offensive usernames
 Accounts.validateNewUser(function (user) {
-    const disallowed = ["root","admin","xxx","fuck","sex","slut","bitch"];
+    const disallowed = ["root","admin"];
     const name = user.profile.name.toLowerCase();
     return !(_.contains(name,disallowed));
 });
@@ -89,3 +92,100 @@ Accounts.validateLoginAttempt(function (attempt) {
     return true;
 });
 
+Accounts.registerLoginHandler('facebook', function(params) {
+    const data = params.facebook;
+    console.log("FB account handler");
+
+    // If this isn't facebook login then we don't care about it. No need to proceed.
+    if (!data) {
+      return undefined;
+    }
+
+    // The fields we care about (same as Meteor's)
+    const whitelisted = ['id', 'email', 'name', 'first_name',
+     'last_name', 'link', 'gender', 'locale', 'age_range'];
+
+    // Get our user's identifying information. This also checks if the accessToken
+    // is valid. If not it will error out.
+    const identity = getIdentity(data.accessToken, whitelisted);
+
+    // Build our actual data object.
+    const serviceData = {
+      accessToken: data.accessToken,
+      expiresAt: (+new Date) + (1000 * data.expirationTime)
+    };
+    const fields = Object.assign({}, serviceData, identity);
+
+    // Search for an existing user with that facebook id
+    let existingUser;
+    if(identity && identity.email && identity.email != null){
+        existingUser = Meteor.users.findOne({'registered_emails.address': identity.email});
+    }else{
+        existingUser = Meteor.users.findOne({ 'services.facebook.id': identity.id });
+    }
+
+    let userId;
+if (existingUser) {
+  userId = existingUser._id;
+  console.log(userId);
+
+
+  // Update our data to be in line with the latest from Facebook
+  const prefixedData = {};
+  _.each(fields, (val, key) => {
+    prefixedData[`services.facebook.${key}`] = val;
+  });
+
+  Meteor.users.update({ _id: userId }, {
+    $set: prefixedData,
+    $addToSet: { emails: { address: identity.email, verified: true } }
+  });
+
+} else {
+  // Create our user
+  console.log("new user");
+
+  userId = Meteor.users.insert({
+    services: {
+      facebook: fields
+    },
+    profile: { name: identity.name,
+        gender: identity.gender,
+        status: 'FB to MB',
+        updatedAt:new Date(),
+        notifications: true,
+        language: identity.locale,
+        friends: [],
+        city: null,
+        country: null,
+        points: 0,
+        avatarUrl: "http://graph.facebook.com/" + identity.id + "/picture/?type=large"
+        },
+    emails: [{
+      address: identity.email,
+      verified: true
+    }]
+  });
+}
+
+return { userId: userId };
+
+});
+
+
+// Gets the identity of our user and by extension checks if
+// our access token is valid.
+const getIdentity = (accessToken, fields) => {
+    try {
+        console.log(accessToken);
+    return HTTP.get("https://graph.facebook.com/v2.4/me", {
+      params: {
+        access_token: accessToken,
+        fields: fields
+      }
+    }).data;
+  } catch (err) {
+    throw _.extend(new Error("Failed to fetch identity from Facebook. " + err.message),
+                   {response: err.response});
+  }
+}
